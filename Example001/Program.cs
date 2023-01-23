72933cceeeade903d5515763d586524d583027a8
Evgenia <janemishkina@yandex.ru>
2023-01-23 18:03:55 +0300

    Напишите программу, которая принимает на вход пятизначное положительное число и является ли оно палиндромом

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -3,9 +3,27 @@
 12821 -> да
 23432 -> да*/
 
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-if (number[0] == number[4] && number[1] == number[3])
+int getCorrectNumber()
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine("Введите пятизначное положительное число: ");
+        if (int.TryParse(Console.ReadLine(), out result) && result>=10000)
+        {
+                break;
+        }        
+        else
+        {
+            Console.WriteLine("Введено некорректное число");
+        }
+    }
+    return result;
+}
+
+int result = getCorrectNumber();
+string number = Convert.ToString(result);
+if(number[0] == number[4] && number[1] == number[3])
 {
     Console.WriteLine("да");
 }
